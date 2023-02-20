bf1cf9e884df656309ecbb0cbf0bc00845479343
Vic <vicliu325@gmail.com>
2023-02-20 19:19:10 +0800

    [add]Barchart & RecentOrders

--- dashboard/pages/index.js
+++ dashboard/pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Header from '@/components/Header'
 import TopCards from '@/components/TopCards'
+import BarChart from '@/components/BarChart'
+import RecentOrders from '@/components/RecentOrders'
 
 export default function Home() {
   return (
@@ -15,6 +17,10 @@ export default function Home() {
       <main className='bg-gray-100 min-h-screen'>
         <Header />
         <TopCards />
+        <div>
+          <BarChart />
+          <RecentOrders />
+        </div>
       </main>
       
     </>
