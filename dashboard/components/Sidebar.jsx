1f1dd869827c6d09822235d1f8239b7b9e2504d6
Vic <vicliu325@gmail.com>
2023-02-20 16:30:14 +0800

    [layout]sidebar

--- dashboard/components/Sidebar.jsx
+++ dashboard/components/Sidebar.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 
-const Sidebar = () => {
+const Sidebar = ({children}) => {
   return (
-    <div>Sidebar</div>
+    <div className='flex'>
+        <div className='fixed w-20 h-screen p-4 bg-white border-r-[1px] flex flex-col justify-between'>
+
+        </div>
+        <main className='ml-20 w-full'>{children}</main>
+    </div>
   )
 }
 
