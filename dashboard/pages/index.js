1b7f8b612ca406913026f8781f5922b4824e5ed4
Vic <vicliu325@gmail.com>
2023-02-20 15:47:46 +0800

    [import]Header inti index.js

--- dashboard/pages/index.js
+++ dashboard/pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Header from '@/components/Header'
 
 export default function Home() {
   return (
@@ -10,8 +11,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className=''>
-
+      <main className='bg-gray-100 min-h-screen'>
+        <Header />
       </main>
     </>
   )
